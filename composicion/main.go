55b2efd4f3cf596b0bb806633a3666c2d66b69ac
Jhamil Antony Crispin Martel <wild@MacBook-Pro-de-Jhamil.local>
2025-08-14 11:21:32 -0500

    Composicion

--- composicion/main.go
+++ composicion/main.go
@@ -1,1 +1,24 @@
-package composicion
+package main
+
+import (
+	"fmt"
+
+	"github.com/jcrispin/go-pdo/composicion/pkg/customer"
+	"github.com/jcrispin/go-pdo/composicion/pkg/invoice"
+	"github.com/jcrispin/go-pdo/composicion/pkg/invoiceitem"
+)
+
+func main() {
+	i := invoice.New(
+		"Spain",
+		"Barcelona",
+		customer.New("John Doe", "123 Main St", "555-1234"),
+		[]invoiceitem.Item{
+			invoiceitem.New(1, "Widget A", 19.99),
+			invoiceitem.New(2, "Widget B", 29.99),
+			invoiceitem.New(3, "Widget C", 9.99),
+		},
+	)
+	i.SetTotal()
+	fmt.Printf("%+v\n", i)
+}
--- composicion/pkg/invoice/invoice.go
+++ composicion/pkg/invoice/invoice.go
@@ -1,10 +1,32 @@
 package invoice
 
-import "github.com/jcrispin/go-pdo/composicion/pkg/customer"
+import (
+	"github.com/jcrispin/go-pdo/composicion/pkg/customer"
+	"github.com/jcrispin/go-pdo/composicion/pkg/invoiceitem"
+)
 
+// estructura de una factura
 type Invoice struct {
 	country string
 	city    string
 	total   float64
 	client  customer.Customer
+	items   []invoiceitem.Item
+}
+
+// Nueva factura
+func New(country, city string, client customer.Customer, items []invoiceitem.Item) Invoice {
+	return Invoice{
+		country: country,
+		city:    city,
+		client:  client,
+		items:   items,
+	}
+}
+
+// SetTotal es e; setter de total de la factura.
+func (i *Invoice) SetTotal() {
+	for _, item := range i.items {
+		i.total += item.Value()
+	}
 }
